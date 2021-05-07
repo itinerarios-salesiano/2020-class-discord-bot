1dde0ec0f4a116b1905539156b892294e33909d4
Lucsvieirabr <62450294+Lucsvieirabr@users.noreply.github.com>
2021-05-07 16:24:58 -0300

    +Comandos

--- comandos/comandos.js
+++ comandos/comandos.js
@@ -6,9 +6,11 @@ function comandos(prefix, msg) {
         .setColor(0x00ff5e)
         .setDescription('Lista de Comandos válidos')
         .setImage('https://i.imgur.com/0dbXdP1.jpeg')
-        .addField(prefix + 'ping', 'Comando Ping')
-        .addField(prefix + 'aula', 'Este comando...')
-        .addField(prefix + 'lvl', 'Este comando retorna as informações do seu level')
+        .addField(`${prefix}ping`, 'Este comando retorna Pong!.')
+        .addField(`${prefix}aula`, 'Este comando manda o link da sua aula do itinerário.')
+        .addField(`${prefix}lvl`, 'Este comando retorna as informações do seu level.')
+        .addField(`${prefix}x1`, 'Este comando te permite tirar um X1 com seu amigo.')
+        .addField(`${prefix}chegae`, 'Este comando faz eu entrar na call e tocar uma musica pog!')
 
 
     msg.channel.send(embed)
